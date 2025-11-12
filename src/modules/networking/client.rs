b261b5ab33331ef70620d1db5d552058aa556429
anonymousomeone <leoistheworst22@gmail.com>
2025-11-12 02:31:49 -0800

    networking discovery wip

--- src/modules/networking/client.rs
+++ src/modules/networking/client.rs
@@ -1,15 +1,22 @@
-use std::net::TcpStream;
+use std::net::{TcpStream, UdpSocket};
+
+use crate::{CrewType, modules::networking::TcpConnection};
 
 pub struct Client {
-    pub connection: TcpStream
+    pub tcp_connection: TcpConnection,
+    pub udp_connection: UdpSocket,
+    pub client_type: CrewType,
 }
 
 impl Client {
-    pub fn new(address: &str) -> Result<Client, std::io::Error> {
-        let connection =  TcpStream::connect(address)?;
+    pub fn new(address: &str, client_type: CrewType) -> Result<Client, std::io::Error> {
+        let tcp_connection =  TcpConnection::new(TcpStream::connect(address)?);
+        let udp_connection = UdpSocket::bind(address)?;
 
         let client = Client { 
-            connection,
+            tcp_connection,
+            udp_connection,
+            client_type,
         };
 
         Ok(client)
--- src/modules/networking/server.rs
+++ src/modules/networking/server.rs
@@ -1,37 +1,52 @@
-use std::net::{SocketAddr, TcpListener, UdpSocket};
+use std::{net::{SocketAddr, TcpListener, TcpStream, UdpSocket}, sync::Mutex};
 
-use crate::modules::networking::{SERVER_ADDR, TcpConnection};
+use crate::{CrewType, modules::networking::{Client, SERVER_ADDR, TcpConnection}};
 
 pub struct Server {
     pub listener: TcpListener,
-    pub tcp_connection: Option<TcpConnection>,
-    pub udp_connection: Option<UdpSocket>,
+    pub clients: Mutex<Vec<Client>>,
+    pub broadcast_socket: UdpSocket,
 }
 
 impl Server {
     pub fn new() -> Result<Server, std::io::Error> {
         let listener = TcpListener::bind(SERVER_ADDR)?;
 
+        let socket = UdpSocket::bind("255.255.255.255").unwrap();
+        socket.set_broadcast(true).expect("set_broadcast call failed");
+
         let server = Server {
             listener,
-            tcp_connection: None,
-            udp_connection: None,
+            clients: Mutex::new(Vec::new()),
+            broadcast_socket: socket,
         };
 
         Ok(server)
     }
 
-    pub fn await_tcp_connection(&mut self) -> Result<SocketAddr, std::io::Error> {
-        let (stream, addr) = self.listener.accept()?;
-        self.tcp_connection = Some(TcpConnection::new(stream));
-
-        Ok(addr)
+    pub fn broadcast_discovery(&self) {
+        let discovery_message = b"HAWK_TUAH_DISCOVERY";
+        self.broadcast_socket.send(discovery_message).unwrap();
     }
 
-    pub fn await_udp_connection(&self, remote_addr: &str) -> Result<(), std::io::Error> {
-        let socket = UdpSocket::bind(SERVER_ADDR)?;
-        socket.connect(remote_addr)?;
-
+    pub fn handle_connections(&mut self) -> Result<(), std::io::Error> {
+        for stream in self.listener.incoming() {
+            match stream {
+                Ok(tcp_stream) => {
+                    let udp_connection = UdpSocket::bind(tcp_stream.peer_addr().unwrap())?;
+                    let client = Client {
+                        tcp_connection: TcpConnection::new(tcp_stream),
+                        udp_connection,
+                        client_type: CrewType::Gunner,
+                    };
+                    self.clients.lock().unwrap().push(client);
+                    println!("New client connected!");
+                }
+                Err(e) => {
+                    println!("Error accepting connection: {}", e);
+                }
+            }
+        }
         Ok(())
     }
 }
